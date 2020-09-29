23a7e0c8d1ef9869e7ede746cce7e4e47dee16eb
asuka.inoue <65387050+asuka-inoue@users.noreply.github.com>
2020-09-29 17:46:33 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,17 +44,44 @@ $(function(){
   });
 });
 
-$(function(){
-  $('.burger-btn').on('click', function() {
-    $(this).slideToggle();
-    $(this).toggleClass('active');
+$('.menu a[href^="#"]').click(function() {
+  
+      var speed = 400;
+  
+      var href= $(this).attr("href");
+  
+      var target = $(href == "#" || href == "" ? 'html' : href);
+  
+      var position = target.offset().top;
+  
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+  
+      return false;
+  
+    });
+  
+    $(function(){
+      $('.burger-btn').on('click', function() {
+        $('header-nav').slideToggle();
+        $(this).toggleClass('active');
+    
+        if ($(this).hasClass('active')) {
+          $('.header-nav').addClass('active');
+      } else {
+          $('.header-nav').removeClass('active');
+      }
+      });
+    });
 
-    if ($(this).hasClass('active')) {
-      $('.header-nav').addClass('active');
-  } else {
-      $('.header-nav').removeClass('active');
-  }
-  });
-});
+
+    $('.header-nav ul li a').on('click', function() {
+      
+          $('.header-nav').slideToggle();
+      
+          $('.header-nav').removeClass('active');
+      
+      });
+      
+
 
 
