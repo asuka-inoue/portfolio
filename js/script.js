b125b92729e36ca9d38ad0f3d91a33ac70dac47f
asuka.inoue <65387050+asuka-inoue@users.noreply.github.com>
2020-09-30 23:57:04 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,6 +82,76 @@ $('#menu a[href^="#"]').click(function() {
       
       });
       
+var $work1 = $('#work1');
+  var $work2 = $('#work2');
+  var $workModal1 = $('#work1-modal');
+  var $workModal2 = $('#work2-modal');
+  var touch_start_y;
+  var $mask = $('.mask');
+  var $close = $('.mask, .close-modal')
+
+    // タッチしたとき開始位置を保存しておく
+    $(window).on('touchstart', function(event) {
+      touch_start_y = event.originalEvent.changedTouches[0].screenY;
+    });
+
+    // つぶやきをクリック
+    $work1.on('click', function() {
+      // スワイプしているとき
+      $(window).on('touchmove.noscroll', function(event) {
+        var current_y = event.originalEvent.changedTouches[0].screenY,
+            height = $workModal1.outerHeight(),
+            is_top = touch_start_y <= current_y && $workModal1[0].scrollTop === 0,
+            is_bottom = touch_start_y >= current_y && $workModal1[0].scrollHeight - $workModal1[0].scrollTop === height;
+        
+        // スクロール対応モーダルの上端または下端のとき
+        if (is_top || is_bottom) {
+          // スクロール禁止
+          event.preventDefault();
+        }
+      });
+      
+      // スクロール禁止
+      $('html, body').css('overflow', 'hidden');
+      
+      // モーダルをフェードイン
+      $workModal1.fadeIn();
+      $mask.fadeIn();
+    });
+
+    // つぶやきをクリック
+    $work2.on('click', function() {
+      // スワイプしているとき
+      $(window).on('touchmove.noscroll', function(event) {
+        var current_y = event.originalEvent.changedTouches[0].screenY,
+            height = $workModal2.outerHeight(),
+            is_top = touch_start_y <= current_y && $workModal2[0].scrollTop === 0,
+            is_bottom = touch_start_y >= current_y && $workModal2[0].scrollHeight - $workModal2[0].scrollTop === height;
+        
+        // スクロール対応モーダルの上端または下端のとき
+        if (is_top || is_bottom) {
+          // スクロール禁止
+          event.preventDefault();
+        }
+      });
+      
+      // スクロール禁止
+      $('html, body').css('overflow', 'hidden');
+      
+      // モーダルをフェードイン
+      $workModal2.fadeIn();
+      $mask.fadeIn();
+    });
+
+    $close.click ( function() {
+      $workModal1.fadeOut();
+      $workModal2.fadeOut();
+      $mask.fadeOut();
+      $('html, body').removeAttr('style');
+      // イベントを削除
+      $(window).off('touchmove.noscroll');
+    });
+}
 
 
 
